dc92d5a765ef3f744236a1b0630d016b4ea9f0df
ttm <taras.tataryn.ttm@gmail.com>
2018-07-28 16:45:33 +0300

    Create main component

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,16 +1,19 @@
-import { HeaderComponent } from './../../components/header/header.component';
 import { NgModule } from '@angular/core';
 
 import { HomeComponent } from './home.component';
+import { HeaderComponent } from './../../components/header/header.component';
+import { MainComponent } from '../../components/main/main.component';
 
 @NgModule({
   declarations: [
 		HomeComponent,
-		HeaderComponent
+		HeaderComponent,
+		MainComponent
 	],
 	exports: [
 		HomeComponent,
-		HeaderComponent
+		HeaderComponent,
+		MainComponent
 	]
 })
 
