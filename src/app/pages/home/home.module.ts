4a5c4b2cd6ec7a07cf397916e72847314a3dc5a4
ttm <taras.tataryn.ttm@gmail.com>
2018-07-28 16:49:56 +0300

    Add sidebar component

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 import { HomeComponent } from './home.component';
 import { HeaderComponent } from './../../components/header/header.component';
 import { MainComponent } from '../../components/main/main.component';
+import { SidebarComponent } from '../../components/main/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
 		HomeComponent,
 		HeaderComponent,
-		MainComponent
+		MainComponent,
+		SidebarComponent
 	],
 	exports: [
 		HomeComponent,
