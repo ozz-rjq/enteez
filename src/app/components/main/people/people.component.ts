62c9ab2bcb25cb9cf7846612468aae278922cbd1
ttm <taras.tataryn.ttm@gmail.com>
2018-08-04 18:01:37 +0300

    Add new components

---
+++ src/app/components/main/people/people.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'app-people',
+	templateUrl: './people.component.html',
+	styleUrls: ['./people.component.scss']
+})
+
+export class PeopleComponent implements OnInit {
+	ngOnInit() {}
+}
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,16 +1,22 @@
+import { FeaturesComponent } from './../../components/main/features/features.component';
+import { PeopleComponent } from './../../components/main/people/people.component';
 import { NgModule } from '@angular/core';
 
 import { HomeComponent } from './home.component';
 import { HeaderComponent } from './../../components/header/header.component';
 import { MainComponent } from '../../components/main/main.component';
 import { SidebarComponent } from '../../components/main/sidebar/sidebar.component';
+import { ServicesComponent } from '../../components/main/services/services.component';
 
 @NgModule({
   declarations: [
 		HomeComponent,
 		HeaderComponent,
 		MainComponent,
-		SidebarComponent
+		SidebarComponent,
+		ServicesComponent,
+		PeopleComponent,
+		FeaturesComponent
 	],
 	exports: [
 		HomeComponent,
