5fcc088d767dad6325a8c509050c2015d795e6f6
ttm <taras.tataryn.ttm@gmail.com>
2018-08-11 14:51:45 +0300

    Add initial graphics

---
+++ src/app/components/main/advertisment/ad.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'app-ad',
+	templateUrl: './ad.component.html',
+	styleUrls: ['./ad.component.scss']
+})
+
+export class AdComponent implements OnInit {
+	ngOnInit() {}
+}
